e33987bb8da0b462d57e717e2dc32d483f437545
Nick Menke <nick@nlmenke.net>
2019-06-19 21:38:57 -0400

    rebuilding from scratch

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 MIT License
 
-Copyright (c) 2018 Nick Menke <nick@nlmenke.net>
+Copyright (c) 2018-2019 Nick Menke <nick@nlmenke.net>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- src/DeployVersionServiceProvider.php
+++ src/DeployVersionServiceProvider.php
@@ -1,12 +1,13 @@
 <?php namespace NLMenke\DeployVersion;
 
 use Illuminate\Support\ServiceProvider;
-use NLMenke\DeployVersion\Console\DeployCommand;
-use NLMenke\DeployVersion\Console\DeployMakeCommand;
-use NLMenke\DeployVersion\Deployments\Deployer;
-use NLMenke\DeployVersion\Deployments\DeploymentCreator;
-use NLMenke\DeployVersion\Deployments\DeploymentRepository;
 
+/**
+ * Class DeployVersionServiceProvider
+ *
+ * @package NLMenke\DeployVersion
+ * @author  Nick Menke <nick@nlmenke.net>
+ */
 class DeployVersionServiceProvider extends ServiceProvider
 {
     /**
@@ -23,22 +24,7 @@ class DeployVersionServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->bootConfig();
-    }
-
-    /**
-     * Register services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->registerConfig();
-        $this->registerRepository();
-        $this->registerDeployer();
-        $this->registerCreator();
-        $this->registerCommands();
-        $this->registerFacade();
+        //
     }
 
     /**
@@ -49,105 +35,17 @@ class DeployVersionServiceProvider extends ServiceProvider
     public function provides(): array
     {
         return [
-            Deployer::class,
-            DeploymentCreator::class,
-            DeploymentRepository::class,
-            DeployVersionService::class,
+            //
         ];
     }
 
     /**
-     * Bootstrap config.
+     * Register services.
      *
      * @return void
      */
-    protected function bootConfig()
+    public function register()
     {
-        $configPath = __DIR__ . '/../config/config.php';
-
-        $this->publishes([
-            $configPath => config_path('deploy-version.php')
-        ], 'config');
-    }
-
-    /**
-     * Register the deployment commands.
-     *
-     * @return void
-     */
-    protected function registerCommands()
-    {
-        $this->commands([
-            DeployMakeCommand::class,
-            DeployCommand::class,
-        ]);
-    }
-
-    /**
-     * Register config.
-     *
-     * @return void
-     */
-    protected function registerConfig()
-    {
-        $configPath = __DIR__ . '/../config/config.php';
-
-        $this->mergeConfigFrom($configPath, 'deploy-version');
-    }
-
-    /**
-     * Register the deployment creator.
-     *
-     * @return void
-     */
-    protected function registerCreator()
-    {
-        $this->app->singleton(DeploymentCreator::class, function ($app) {
-            return new DeploymentCreator($app['files']);
-        });
-    }
-
-    /**
-     * Register the deployer service.
-     *
-     * @return void
-     */
-    protected function registerDeployer()
-    {
-        $this->app->singleton(Deployer::class, function ($app) {
-            $repository = $app[DeploymentRepository::class];
-
-            return new Deployer($repository, $app['db'], $app['files']);
-        });
-    }
-
-    /**
-     * Register the facade.
-     *
-     * @return void
-     */
-    protected function registerFacade()
-    {
-        $this->app->singleton(DeployVersionService::class, function ($app) {
-            $repository = $app[DeploymentRepository::class];
-
-            return new DeployVersionService($repository);
-        });
-
-        $this->app->alias(DeployVersionService::class, 'deploy-version');
-    }
-
-    /**
-     * Register the deployment repository service.
-     *
-     * #return void
-     */
-    protected function registerRepository()
-    {
-        $this->app->singleton(DeploymentRepository::class, function ($app) {
-            $table = $app['config']['deploy-version.table'];
-
-            return new DeploymentRepository($app['db'], $table);
-        });
+        //
     }
 }
